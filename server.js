6a235fe74c8401ec0c3f00b5962c5e8712868114
iam-mwaseem <dev.muhammad.waseem@gmail.com>
2024-06-04 17:20:04 +0500

    simple-node uploaded

---
+++ server.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+app.use("/", (req, res) => {
+  res.send("<h1>Hi!! from your simple node server</h1>");
+});
+
+mongoose.connect(process.env.DATABASE_URI).then(() => {
+  console.log("Connected to MongoDB");
+  app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+  });
+});
